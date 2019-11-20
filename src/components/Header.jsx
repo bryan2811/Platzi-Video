9a9d478868df6890feee67941872f6ede917ee88
Bryan <brandonb2816@gmail.com>
2019-11-20 13:47:41 -0400

    Agregando imagenes y reutilizando componentes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss';
 
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/user-icon.png';
+
 const Header = () => (
     
     <header className="header">
-        <img tabIndex="0" className="header__img" src="../assets/logo-platzi-video-BW2.png" alt="Platzi Video" />
+        <img tabIndex="0" className="header__img" src={logo} alt="Platzi Video" />
 
         <div className="header__menu">
             
             <div className="header__menu--profile">
-                <img src="../assets/user-icon.png" alt="User" />
+                <img src={userIcon} alt="User" />
                 <p tabIndex="0">Perfil</p>
             </div>
 
