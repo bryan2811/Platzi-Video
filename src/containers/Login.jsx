2d9aed077a7fd42b068b9d8dac69c385df38d002
Bryan <brandonb2816@gmail.com>
2019-12-03 13:35:03 -0400

    Adding login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,51 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginRequest } from '../actions';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login = () => (
-  <section className='login'>
+const Login = props => {
 
-    <section className='login__container'>
-      <h2 tabIndex='0'>Inicia sesión</h2>
+  const [form, setValues] = useState({
+    email: '',
+  });
 
-      <form className='login__container--form' action='../maquetacion-principal.html'>
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-        <input aria-label='Correo' type='text' className='input' placeholder='Correo' />
-        <input aria-label='Contraseña' className='input' type='password' contraseña='Contraseña' placeholder='Contraseña' />
-        <button className='button'>Iniciar Sesión</button>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.loginRequest(form);
+    props.history.push('/');
+  };
 
-        <div className='login__container--remember-me'>
-          <label>
-            <input type='checkbox' name='' id='cbox1' value='checkbox' />
-Recuérdame
-          </label>
-          <a href='/'>Olvidé mi contraseña</a>
-        </div>
+  return (
 
-      </form>
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia sesión</h2>
 
-      <section className='login__container--social-media'>
-        <div>
-          <img src={googleIcon} alt='Google' />
-Inicia sesión con Google
-        </div>
-        <div>
-          <img src={twitterIcon} alt='Twitter' />
-Inicia sesión con Twitter
-        </div>
-      </section>
-      <p className='login__container--register'>
+        <form
+          className='login__container--form'
+          onSubmit={handleSubmit}
+        >
+          <input
+            name='email'
+            aria-label='Correo'
+            type='text'
+            className='input'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            aria-label='Contraseña'
+            className='input'
+            type='password'
+            contraseña='Contraseña'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>Iniciar Sesión</button>
+
+          <div className='login__container--remember-me'>
+            <label>
+              <input type='checkbox' name='' id='cbox1' value='checkbox' />
+            Recuérdame
+            </label>
+            <a href='/'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} alt='Google' />
+          Inicia sesión con Google
+          </div>
+          <div>
+            <img src={twitterIcon} alt='Twitter' />
+          Inicia sesión con Twitter
+          </div>
+        </section>
+        <p className='login__container--register'>
         No tienes ninguna cuenta?
-        <Link to='/register'>
-        Regístrate
-        </Link>
-      </p>
-
+          <Link to='/register'>Regístrate</Link>
+        </p>
+      </section>
     </section>
+  );
+};
 
-  </section>
-);
+const mapDispatchToProps = {
+  loginRequest,
+}
 
-export default Login;
+export default connect(null, mapDispatchToProps)(Login);
